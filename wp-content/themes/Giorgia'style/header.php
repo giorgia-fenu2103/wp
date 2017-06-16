036b42babed9a1dff3a7d1a8608ebf466ddd7269
giorgia-fenu2103 <giorgiafn478@gmail.com>
2017-06-16 19:16:16 +0200

    Add files via upload

---
+++ wp-content/themes/Giorgia'style/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php wp_head(); ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+</head>
+<body>
+	<div class="jumbotron">
+		<div class="container">
+			<nav class="navbar navbar-default navbar fixed-top">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-left">
+							<li><a href="#products">Our Products</a></li>
+							<li><a href="#contacts">Contacts</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+			

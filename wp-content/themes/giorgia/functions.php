4f8d1a98f24e8185ee3916a7eb0e785b16a9b0ac
giorgia-fenu2103 <giorgiafn478@gmail.com>
2017-06-20 11:22:04 +0200

    Update functions.php

--- wp-content/themes/giorgia/functions.php
+++ wp-content/themes/giorgia/functions.php
@@ -1,7 +1,7 @@
 <?php
 function enqueue_css() {
     wp_enqueue_style( 'bs', get_template_directory_uri().'/vendors/css/bootstrap.min.css' );
-    wp_enqueue_style( 'st', get_template_directory_uri().'/css/style.css' );
+    wp_enqueue_style( 'st', get_template_directory_uri().'/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_css' );
 function enqueue_js() {
